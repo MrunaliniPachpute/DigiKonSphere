9e3f2422903ff0d1d6f036ad84b0df17ee83ec27
MrunaliniPachpute <pachputemrunalini@gmail.com>
2025-10-16 05:09:29 +0530

    cleared repo

--- public/JS/iotSimulation.js
+++
@@ -1,9 +0,0 @@
-// Simple LED simulation
-const statusEl = document.getElementById('status');
-if (statusEl) {
-    if (statusEl.innerText === 'success') {
-        statusEl.style.color = 'green';
-    } else {
-        statusEl.style.color = 'red';
-    }
-}
